1aaa08d1c9654a81b4f75324a865c12a2992cbf0
Tobias <tobiasvega1210@gmail.com>
2024-09-02 12:26:16 -0300

    Se modularizó backend-session

---
+++ backend-session/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import { database } from "../db/database.js";
+
+// Ruta para manejar el inicio de sesión
+export const login =  (req, res) => {
+    const { username, password } = req.body;
+
+    // Buscar usuario
+    const user = database.user.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        // Guardar información del usuario en la sesión
+        req.session.userId = user.id;
+        req.session.username = user.username;
+
+        return res.json({ 
+            message: 'Inicio de sesión exitoso', 
+            user: { id: user.id, username: user.username } });
+    } else {
+        return res.status(401).json({ message: 'Credenciales incorrectas' });
+    }
+};
+
+// Ruta para obtener los datos de la sesión
+export const session = (req, res) => {
+    if (req.session.userId) {
+        return res.json({ 
+            loggedIn: true, 
+            user: { id: req.session.userId, username: req.session.username } });
+    } else {
+        return res.status(401).json({ loggedIn: false, message: 'No hay sesión activa' });
+    }
+};
+
+// Ruta para cerrar la sesión
+export const logout = (req, res) => {
+    console.log(req.session)
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).json({ message: 'Error al cerrar la sesión' });
+        }
+        res.clearCookie('connect.sid'); // Nombre de cookie por defecto para express-session
+        return res.json({ message: 'Sesión cerrada exitosamente' });
+    });
+};
