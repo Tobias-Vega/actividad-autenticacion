a2b4eac9b8ebf98c77280cb723e6a9cfa3458ae9
Tobias <tobiasvega1210@gmail.com>
2024-09-04 11:14:54 -0300

    Se elliminó el directorio de backend-session

--- backend-session/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, session, logout,register } from "../controllers/auth.controller.js";
-
-export const authRouter = Router();
-
-authRouter.post('/register', register)
-authRouter.post('/login', login);
-authRouter.get('/session', session);
-authRouter.post('/logout', logout);
